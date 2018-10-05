95e2f32e142510a2d36db08d871ac26bb2fce8b8
Yang Xu <yxu@ca.com>
2018-10-05 18:02:14 -0400

    2018-10-05

--- problem2/contracts.py
+++ problem2/contracts.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import sqlite3
 
@@ -11,8 +10,7 @@ sheets = xls.sheet_names
 #Keep only Federal and Department data
 sheets_toKeep = ['Federal']
 
-for sheet in sheets:
-    print (sheet)
+for sheet in sheets:    
     if "00" in str(sheet): sheets_toKeep.append(sheet)
     
 #Read Federal and Department data to pd frame    
@@ -88,64 +86,4 @@ conn.commit()
 cur.close()
 conn.close()
 
-
-
-
-contractor_id_department_count = actions[["Department","contractor_id"]].groupby(["contractor_id"]).agg(['count'])
-
-contractor_id_department_count.columns =["departments"]
-
-contractor_id_department_count["contractor_id"] = contractor_id_department_count.index
- 
-contractor_id_department_counts = contractor_id_department_count.groupby(["departments"]).agg(['count'])
-
-contractor_id_department_counts.columns =["Vendors"]
-total_vendors = contractor_id_department_counts.sum()[0]
-contractor_id_department_counts["Vendors"] =  contractor_id_department_counts["Vendors"] /total_vendors
-
-
-import numpy as np
-import random as py_random
-import numpy.random as np_random
-import time
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-
-THEME = "darkslategray"
-
-contractor_id_department_counts.loc[15] = 0 
-contractor_id_department_counts.loc[19] = 0 
-
-contractor_id_department_counts = contractor_id_department_counts.sort_index()
-
-percent = contractor_id_department_counts["Vendors"]
-departments =  contractor_id_department_counts.index
-width = 1/2
-
-figure = plt.figure(figsize=(10, 6)) # first element is width, second is height.
-
-axes = figure.add_subplot(1, 1, 1)
-
-axes.set_title( "Vendors Received Contracts")
-axes.bar(range(0, len(percent)), percent, width, color=THEME, align="center")
-axes.set_xticks(range(0, len(percent)))
-axes.set_xticklabels(departments)
-axes.yaxis.grid( b=True, which="major")
-axes.set_ylim((0, 1))
-axes.set_ylabel( "Percent of Total Vendors")
-axes.set_xlabel( "Departments")
-
-rects = axes.patches
-
-# Make some labels.
-labels = round(percent*100,1)
-
-for rect, label in zip(rects, labels):
-    height = rect.get_height()
-    axes.text(rect.get_x() + rect.get_width() / 2, height + 0.1, label,
-            ha='center', va='bottom')
-    
-plt.show()
-
-
+print ("Done!")
